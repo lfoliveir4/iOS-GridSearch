054bf95a6ba6163cd189d0ceab1c5ad815aef253
Luis Filipe Alves de Oliveira <lfoliveira.dev@gmail.com>
2021-06-15 21:39:06 -0300

    Initial Commit

---
+++ GridSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GridSearch
+//
+//  Created by Luis Filipe Alves de Oliveira on 15/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridSearch/GridSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSearchApp.swift
+//  GridSearch
+//
+//  Created by Luis Filipe Alves de Oliveira on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct GridSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
